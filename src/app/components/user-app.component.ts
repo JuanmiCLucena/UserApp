e249351e13e45616b4d82911b7e2c70e5fe8198c
Juanmi <jmcostalucena2000@gmail.com>
2025-06-10 13:08:35 +0200

    Aplicación de usuarios. Componente principal user-app y componente user (lista usuarios), además del user-service han sido generados. (También el modelo)

---
+++ src/app/components/user-app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { UserComponent } from './user/user.component';
+
+@Component({
+  selector: 'user-app',
+  imports: [UserComponent],
+  templateUrl: './user-app.component.html'
+})
+export class UserAppComponent implements OnInit{
+
+  title: string = 'Listado de usuarios!';
+
+  users: User[] = [];
+
+  constructor(private service: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.service.findAll().subscribe( users => this.users = users );
+  }
+
+}
