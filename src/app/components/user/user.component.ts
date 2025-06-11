bc6afcedada4a73c5b7b8d1d40991de285aa47ac
Juanmi <jmcostalucena2000@gmail.com>
2025-06-11 12:43:15 +0200

    Eliminamos input() y output() en UserComponent, recibimos los usuarios en NavbarComponent desde UserAppComponent

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter} from '@angular/core';
 import { User } from '../../models/user';
 import { RouterModule } from '@angular/router';
 
@@ -11,11 +11,11 @@ export class UserComponent {
 
   title: string = 'Listado de usuarios!';
 
-  @Input() users: User[] = [];
+  users: User[] = [];
 
-  @Output() idUserEventEmitter: EventEmitter<number> = new EventEmitter();
+  idUserEventEmitter: EventEmitter<number> = new EventEmitter();
 
-  @Output() selectedUserEventEmitter: EventEmitter<User> = new EventEmitter();
+  selectedUserEventEmitter: EventEmitter<User> = new EventEmitter();
 
   selectedUser(user: User): void {
     this.selectedUserEventEmitter.emit(user);
